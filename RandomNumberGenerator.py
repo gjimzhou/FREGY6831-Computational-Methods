a084ea90aecb7d0b73aee9cb437aa61036796c29
Jimmy <gjimzhou@gmail.com>
2026-09-22 23:10:52 -0400

    Fix Marsaglia-Bray odd sample handling

--- RandomNumberGenerator.py
+++ RandomNumberGenerator.py
@@ -33,18 +33,21 @@ def Marsaglia_Bray_Generator(N, seed=1):
     M = len(standard_gaussian_random_number)
 
     while M < N:
-        uniform_random_number = Linear_Congruential_Generator(N - M, seed)
+        remaining = N - M
+        draws = remaining + remaining % 2
+        uniform_random_number = Linear_Congruential_Generator(draws, seed)
         seed = int(uniform_random_number[-1] * 2147483647)
 
-        for i in range(int((N - M) / 2)):
+        for i in range(draws // 2):
             u1 = 2 * uniform_random_number[2 * i] - 1
             u2 = 2 * uniform_random_number[2 * i + 1] - 1
             x = u1 ** 2 + u2 ** 2
 
-            if x <= 1:
+            if 0 < x <= 1:
                 y = math.sqrt(-2 * math.log(x) / x)
                 standard_gaussian_random_number.append(u1 * y)
-                standard_gaussian_random_number.append(u2 * y)
+                if len(standard_gaussian_random_number) < N:
+                    standard_gaussian_random_number.append(u2 * y)
 
         M = len(standard_gaussian_random_number)
 
